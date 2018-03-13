ffd669e1f33ee798d96ed92312887ed1efbe9c8e
AlphaGo <gouwantong@live.com>
2018-03-13 22:22:23 +0800

    update

--- src/components/ImageItem.ts
+++ src/components/ImageItem.ts
@@ -3,22 +3,33 @@ export type Position = {
   y: number,
 };
 
+export type ActionPosition = {
+  start?: Position,
+  move?: Position,
+  end?: Position,
+};
+
 export type ImageItemConfig = {
-  position?: Position,
-  shape?: Array<Position>,
+  imgSrc: string,
+  position: Position,
+  shape: Array<Position>,
 };
 
 export default class ImageItem {
+  axisOrigin: Position;
   isLoadedImage: boolean = false;
-  imgSrc: string;
   image: HTMLImageElement;
   imageConfig: ImageItemConfig;
   ctx: CanvasRenderingContext2D;
+  movingThumbtackIndex: number | null = null; 
 
-  constructor(ctx: CanvasRenderingContext2D, imgSrc: string, config: ImageItemConfig = {}) {
-    this.imgSrc = imgSrc;
+  // private isMoving = false;
+  private thumbtackRadius = 20;
+
+  constructor(ctx: CanvasRenderingContext2D, canvasAxisOrigin: Position, config: ImageItemConfig) {
     this.ctx = ctx;
     this.imageConfig = config;
+    this.axisOrigin = canvasAxisOrigin;
   }
 
   init() {
@@ -27,34 +38,95 @@ export default class ImageItem {
   }
 
   render() {
-    // tslint:disable-next-line
-    // console.log('render'); 
+    if (this.isLoadedImage) {
+        this.drawImage();
+    }
+    this.drawThumbtack();
+    this.clip();
+  }
+
+  changeShape(position: ActionPosition) {
+    this.ctx.clearRect(0, 0, 300, 300);
+    const { start, move, end } = position;
+    if (start) {
+      const {x, y} = start;
+      let curAxisX = x - this.axisOrigin.x;
+      let curAxisY = y - this.axisOrigin.y;
+      for (let i = 0; i < this.imageConfig.shape.length; i++) {
+        const curThumbtack = this.imageConfig.shape[i];
+        if (
+          ((curAxisX - curThumbtack.x) ** 2 + (curAxisY - curThumbtack.y) ** 2) < this.thumbtackRadius ** 2
+        ) {
+          this.movingThumbtackIndex = i;
+          return;
+        }
+      }
+    }
+    if (move) {
+      if (this.movingThumbtackIndex === null) {
+        return;
+      } else {
+        this.imageConfig.shape[this.movingThumbtackIndex] = {
+          x: move.x - this.axisOrigin.x,
+          y: move.y - this.axisOrigin.y,
+        };
+        return;
+      }
+    }
+    if (end) {
+      if (this.movingThumbtackIndex === null) {
+        return;
+      } else {
+        this.imageConfig.shape[this.movingThumbtackIndex] = {
+          x: end.x - this.axisOrigin.x,
+          y: end.y - this.axisOrigin.y,
+        };
+        this.movingThumbtackIndex = null;
+        return;
+      }
+    }
+  }
+
+  private drawThumbtack() {
+    const { shape } = this.imageConfig;
+    shape.map((position) => {
+      this.ctx.beginPath();
+      this.ctx.arc(position.x, position.y, this.thumbtackRadius, 0, Math.PI * 2);
+      this.ctx.fillStyle = 'rgba(0,0,0,0.6)';
+      this.ctx.fill();
+    });
+  }
+
+  private clip() {
+    const { shape } = this.imageConfig;
+    if (shape && shape.length > 2) {
+      this.ctx.beginPath();
+      this.ctx.moveTo(shape[0].x, shape[0].y);
+      for (let i = 1; i < shape.length; i++) {
+        const {x, y} = shape[i];
+        this.ctx.lineTo(x, y);
+      }
+      this.ctx.closePath();
+      // this.ctx.stroke();
+      this.ctx.clip();
+    }
+  }
+
+  private drawImage() {
+    const { position } = this.imageConfig;
+    if (position) {
+      this.ctx.drawImage(this.image, position.x, position.y);
+    } else {
+      this.ctx.drawImage(this.image, 0, 0);
+    }
   }
 
   private loadImage() {
     const image = new Image();
-    image.src = this.imgSrc;
+    image.src = this.imageConfig.imgSrc;
     image.onload = () => {
       this.isLoadedImage = true;
       this.image = image;
-
-      const { position, shape } = this.imageConfig;
-      if (shape && shape.length > 2) {
-        this.ctx.beginPath();
-        this.ctx.moveTo(shape[0].x, shape[0].y);
-        for (let i = 1; i < shape.length; i++) {
-          const {x, y} = shape[i];
-          this.ctx.lineTo(x, y);
-        }
-        this.ctx.closePath();
-        this.ctx.stroke();
-        this.ctx.clip();
-      }
-      if (position) {
-        this.ctx.drawImage(this.image, position.x, position.y);
-      } else {
-        this.ctx.drawImage(this.image, 0, 0);
-      }
     };
   }
 
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -9,20 +9,24 @@ export interface SceneProps {
 
 class Scene extends React.Component<SceneProps> {
   canvasRef: HTMLCanvasElement | null;
+  imageItem: ImageItem;
 
   componentDidMount() {
     const src = this.props.images[0];
     const ctx = this.canvasRef!.getContext('2d');
+    const box = this.canvasRef!.getBoundingClientRect();
+    this.startEventListener();
     const imageItem = new ImageItem(
       ctx!,
-      src,
+      {x: box.left, y: box.top},
       {
         position: {x: 0, y: 0},
-        shape: [{x: 150, y: 0}, {x: 300, y: 300}, {x: 150, y: 600}]
+        shape: [{x: 150, y: 20}, {x: 30, y: 300}, {x: 150, y: 600}],
+        imgSrc: src,
       },
     );
+    this.imageItem = imageItem;
     imageItem.init();
-
     const render = () => {
       imageItem.render();
       requestAnimationFrame(render);
@@ -30,8 +34,43 @@ class Scene extends React.Component<SceneProps> {
     render();
   }
 
+  startEventListener = () => {
+    if (this.canvasRef) {
+      this.canvasRef.addEventListener('touchstart', (e) => {
+        if (this.imageItem) {
+          this.imageItem.changeShape({
+            start: {
+              x: e.touches[0].clientX,
+              y: e.touches[0].clientY,
+            }
+          });
+        }
+      });
+      this.canvasRef.addEventListener('touchmove', (e) => {
+        if (this.imageItem) {
+          this.imageItem.changeShape({
+            move: {
+              x: e.touches[0].clientX,
+              y: e.touches[0].clientY,
+            }
+          });
+        }
+      });
+      this.canvasRef.addEventListener('touchend', (e) => {
+        if (this.imageItem) {
+          this.imageItem.changeShape({
+            end: {
+              x: e.changedTouches[0].clientX,
+              y: e.changedTouches[0].clientY,
+            }
+          });
+        }
+      });
+    }
+  }
+
   render() {
-    const { width, height } = this.props; 
+    const { width, height } = this.props;
     return (
       <canvas
         width={width}
